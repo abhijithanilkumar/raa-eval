ff7bd980a943c8e2e9fed4bbcf18e6f5f7eac251
S Giritheja <s.giritheja@gmail.com>
2016-10-19 02:26:15 +0530

    basic topology setup

--- examples/raa-eval-dense.cc
+++ examples/raa-eval-dense.cc
@@ -23,7 +23,59 @@ CalculateThroughput ()
   Simulator::Schedule (MilliSeconds (100), &CalculateThroughput);
 }
 
+void
+experiment (bool enableCtsRts)
+{
+  // 0. Enable or disable CTS/RTS
+  UintegerValue ctsThr = (enableCtsRts ? UintegerValue (100) : UintegerValue (2200));
+  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", ctsThr);
 
+  // 1. Create 94 nodes (dense) 
+  NodeContainer nodes;
+  nodes.Create (94);
+
+  // 2. Place nodes somehow, this is required by every wireless simulation
+  for (size_t i = 0; i < 94; ++i)
+    {
+      nodes.Get (i)->AggregateObject (CreateObject<ConstantPositionMobilityModel> ());
+    }
+
+  // 3. Create propagation loss matrix
+  Ptr<MatrixPropagationLossModel> lossModel = CreateObject<MatrixPropagationLossModel> ();
+  lossModel->SetDefaultLoss (200); // set default loss to 200 dB (no link)
+  for (size_t i = 0; i < 47; ++i)
+  {
+    lossModel->SetLoss (nodes.Get (i)->GetObject<MobilityModel>(), nodes.Get (i+1)->GetObject<MobilityModel>(), 50); // set symmetric loss i <-> i+1 to 50 dB
+  }
+  for (size_t i = 47; i < 93; ++i)
+  {
+  lossModel->SetLoss (nodes.Get (i+1)->GetObject<MobilityModel>(), nodes.Get (i)->GetObject<MobilityModel>(), 50); // set symmetric loss i+1 <-> i to 50 dB
+  }
+
+  // 4. Create & setup wifi channel
+  Ptr<YansWifiChannel> wifiChannel = CreateObject <YansWifiChannel> ();
+  wifiChannel->SetPropagationLossModel (lossModel);
+  wifiChannel->SetPropagationDelayModel (CreateObject <ConstantSpeedPropagationDelayModel> ());
+
+  // 5. Install wireless devices
+  WifiHelper wifi;
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", 
+                                "DataMode",StringValue ("DsssRate2Mbps"), 
+                                "ControlMode",StringValue ("DsssRate1Mbps"));
+  YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel);
+  WifiMacHelper wifiMac;
+  wifiMac.SetType ("ns3::AdhocWifiMac"); // use ad-hoc MAC
+  NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, nodes);
+
+  // 6. Install TCP/IP stack & assign IP addresses
+  InternetStackHelper internet;
+  internet.Install (nodes);
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.0.0.0", "255.0.0.0");
+  ipv4.Assign (devices);
+}
 int 
 main (int argc, char *argv[])
 {
