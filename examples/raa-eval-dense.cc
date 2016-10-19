fad2e43f8189fa16e94ebec2f0443671eb3885af
S Giritheja <s.giritheja@gmail.com>
2016-10-19 14:38:18 +0530

    Added flow monitoring

--- examples/raa-eval-dense.cc
+++ examples/raa-eval-dense.cc
@@ -32,7 +32,7 @@ experiment (bool enableCtsRts)
 
   // 1. Create 94 nodes (dense) 
   NodeContainer nodes;
-  nodes.Create (94);
+  nodes.Create (32);
 
   // 2. Place nodes somehow, this is required by every wireless simulation
   for (size_t i = 0; i < 94; ++i)
@@ -75,7 +75,48 @@ experiment (bool enableCtsRts)
   Ipv4AddressHelper ipv4;
   ipv4.SetBase ("10.0.0.0", "255.0.0.0");
   ipv4.Assign (devices);
+
+  //7.
+
+   // 8. Install FlowMonitor on all nodes
+  FlowMonitorHelper flowmon;
+  Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
+
+  // 9. Run simulation for 10 seconds
+  Simulator::Stop (Seconds (10));
+  Simulator::Run ();
+
+  // 10. Print per flow statistics
+  monitor->CheckForLostPackets ();
+  Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier ());
+  FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats ();
+  for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin (); i != stats.end (); ++i)
+    {
+      // first 2 FlowIds are for ECHO apps, we don't want to display them
+      //
+      // Duration for throughput measurement is 9.0 seconds, since 
+      //   StartTime of the OnOffApplication is at about "second 1"
+      // and 
+      //   Simulator::Stops at "second 10".
+      if (i->first > 2)
+        {
+          Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow (i->first);
+          std::cout << "Flow " << i->first - 2 << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")\n";
+          std::cout << "  Tx Packets: " << i->second.txPackets << "\n";
+          std::cout << "  Tx Bytes:   " << i->second.txBytes << "\n";
+          std::cout << "  TxOffered:  " << i->second.txBytes * 8.0 / 9.0 / 1000 / 1000  << " Mbps\n";
+          std::cout << "  Rx Packets: " << i->second.rxPackets << "\n";
+          std::cout << "  Rx Bytes:   " << i->second.rxBytes << "\n";
+          std::cout << "  Throughput: " << i->second.rxBytes * 8.0 / 9.0 / 1000 / 1000  << " Mbps\n";
+        }
+    }
+
+  // 11. Cleanup
+  Simulator::Destroy ();
+
+
 }
+
 int 
 main (int argc, char *argv[])
 {
